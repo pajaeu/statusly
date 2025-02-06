ba2e37aa1903da7b0ebe6b43969925843c381927
Paul <fgocsgo@gmail.com>
2025-02-06 20:38:07 +0100

    feat: change current project to latest one when deleting current

--- app/Http/Controllers/Project/SwitchProjectController.php
+++ app/Http/Controllers/Project/SwitchProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Project;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 
 class SwitchProjectController extends Controller
 {
--- app/Livewire/Project/ListProject.php
+++ app/Livewire/Project/ListProject.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Project;
 
 use App\Models\Project;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class ListProject extends Component
@@ -10,14 +11,29 @@ class ListProject extends Component
 	public function delete(int $id)
 	{
 		$project = Project::findOrFail($id);
+		$user = auth()->user();
 
-		if (!auth()->user()->hasProject($project)) {
+		if (!$user->hasProject($project)) {
 			abort(403);
 		}
 
-		$project->delete();
+		DB::beginTransaction();;
 
-		$this->dispatch('projects-updated');
+		try {
+			$project->delete();
+
+			if ($user->current_project_id === $project->id) {
+				$user->update([
+					'current_project_id' => $user->projects()->latest()->first()->id ?? null,
+				]);
+			}
+
+			DB::commit();
+
+			$this->dispatch('projects-updated');
+		} catch (\Exception $e) {
+			DB::rollback();
+		}
 	}
 
 	public function render()
