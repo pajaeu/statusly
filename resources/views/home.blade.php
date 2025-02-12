146ff7cb92cfb62f71e5c23040c1b676fd1f5ec2
Paul <fgocsgo@gmail.com>
2025-02-12 12:13:43 +0100

    feat: add features info to lp

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,6 +42,72 @@
             <img src="{{ asset('img/screenshot.png') }}" alt="Statusly screenshot" class="w-full rounded-xl">
         </div>
     </div>
+    <div class="w-full px-7 pb-16 md:pb-20 lg:pb-28 max-w-7xl mx-auto">
+        <div class="text-center mb-12">
+            <h2 class="mb-6 text-4xl font-bold text-slate-900">Our features</h2>
+            <p class="text-slate-500">Stay informed about your service status with real-time monitoring and instant alerts.</p>
+        </div>
+        <div class="flex flex-col md:flex-row gap-5 mb-10">
+            <div class="w-full md:w-1/3">
+                <div class="h-full">
+                    <div class="text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 text-orange-500 mx-auto mb-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25" />
+                        </svg>
+                        <h3 class="mb-2 text-xl font-semibold text-slate-800">Public Status Page</h3>
+                        <p class="text-sm text-slate-500">Share a sleek status page with customers.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full md:w-1/3">
+                <div class="h-full">
+                    <div class="text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 text-orange-500 mx-auto mb-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
+                        </svg>
+                        <h3 class="mb-2 text-xl font-semibold text-slate-800">Real-Time Monitoring</h3>
+                        <p class="text-sm text-slate-500">Automatically checks your service health every minute.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full md:w-1/3">
+                <div class="h-full">
+                    <div class="text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 text-orange-500 mx-auto mb-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+                        </svg>
+                        <h3 class="mb-2 text-xl font-semibold text-slate-800">Automated Incident Logging</h3>
+                        <p class="text-sm text-slate-500">Records service downtimes automatically.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex justify-center gap-5">
+            <div class="w-full md:w-1/3">
+                <div class="h-full">
+                    <div class="text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 text-orange-500 mx-auto mb-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        </svg>
+                        <h3 class="mb-2 text-xl font-semibold text-slate-800">Open-Source</h3>
+                        <p class="text-sm text-slate-500">Fully customizable with open-source flexibility.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full md:w-1/3">
+                <div class="h-full">
+                    <div class="text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 text-orange-500 mx-auto mb-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" />
+                        </svg>
+                        <h3 class="mb-2 text-xl font-semibold text-slate-800">Developer API</h3>
+                        <p class="text-sm text-slate-500">Access your service status via an API for full automation.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="w-full px-7 pb-10 lg:pb-20 max-w-7xl mx-auto">
         <div class="text-center mb-12">
             <h2 class="mb-6 text-4xl font-bold text-slate-900">🚀 Built with Cutting-Edge Technology</h2>
