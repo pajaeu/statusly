d5123010d6778f0c0fef19632a748454d5a59b45
Paul <fgocsgo@gmail.com>
2025-02-13 13:10:41 +0100

    feat: add incident api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\IncidentController;
 use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\TestController;
 use Illuminate\Support\Facades\Route;
@@ -8,5 +9,6 @@ Route::group(['as' => 'api.'], function () {
 	Route::get('/', TestController::class)->name('test');
 
 	Route::apiResource('services', ServiceController::class);
+	Route::apiResource('incidents', IncidentController::class);
 });
 
